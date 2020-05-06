87ea40bd609b137e1ca7767145d268e38a840204
Yougi3 <younes.aassila@outlook.fr>
2020-05-06 20:38:50 +0200

    Correction d'une faute d'orthographe dans le message de la notification

--- background-script.js
+++ background-script.js
@@ -4,7 +4,7 @@ browser.runtime.onMessage.addListener(function(message) {
       "type": "basic",
       "iconUrl": browser.extension.getURL("icons/icon-48.png"),
       "title": "⚠️ Nouveau sondage disponible !",
-      "message": "Un nouveau sondage est disponible sur la classe virtuelle. Penses à y répondre !"
+      "message": "Un nouveau sondage est disponible sur la classe virtuelle. Pense à y répondre !"
     });
   } else {
     browser.notifications.clear("pollNotifierNotification");
