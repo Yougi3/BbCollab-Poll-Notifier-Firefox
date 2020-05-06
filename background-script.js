9b4f08add6e50117a6fd2550c9b85b5edc872ae4
Yougi3 <younes.aassila@outlook.fr>
2020-05-06 15:45:14 +0200

    Initial commit

---
+++ background-script.js
@@ -0,0 +1,12 @@
+browser.runtime.onMessage.addListener(function(message) {
+  if (message.poll) {
+    browser.notifications.create("pollNotifierNotification", {
+      "type": "basic",
+      "iconUrl": browser.extension.getURL("icons/icon-48.png"),
+      "title": "⚠️ Nouveau sondage disponible !",
+      "message": "Un nouveau sondage est disponible sur la classe virtuelle. Penses à y répondre !"
+    });
+  } else {
+    browser.notifications.clear("pollNotifierNotification");
+  }
+});
---
+++ content-script.js
@@ -0,0 +1,21 @@
+var notificationSent = false;
+var interval = 5000; // In milliseconds
+
+window.setInterval(function() {
+  var pollElement = document.getElementById("poll-popup");
+  if (pollElement) {
+    // Trigger the notification if the poll popup is visible on screen.
+    if (!pollElement.classList.contains("ng-hide")) {
+      // Send a notification only if no previous notifications were sent.
+      if (!notificationSent) {
+        notificationSent = true;
+        browser.runtime.sendMessage({"poll": true});
+      }
+    } else {
+      if (notificationSent) {
+        notificationSent = false;
+        browser.runtime.sendMessage({"poll": false});
+      }
+    }
+  }
+}, interval);
