b787f8d3746a32f7c2a40ea0f3dcec4b622a2d2a
Yougi3 <younes.aassila@outlook.fr>
2020-05-25 20:29:17 +0200

    Support de l'extension 'Notification Sound' de freaktechnik

--- background-script.js
+++ background-script.js
@@ -1,12 +1,18 @@
+var notificationId = "poll-notifier-notification";
+
+browser.notifications.onShown.addListener(function() {
+  browser.runtime.sendMessage("@notification-sound", "new-notification");
+});
+
 browser.runtime.onMessage.addListener(function(message) {
   if (message.poll) {
-    browser.notifications.create("pollNotifierNotification", {
+    browser.notifications.create(notificationId, {
       "type": "basic",
       "iconUrl": browser.extension.getURL("icons/icon-48.png"),
       "title": "⚠️ Nouveau sondage disponible !",
       "message": "Un nouveau sondage est disponible sur la classe virtuelle. Pense à y répondre !"
     });
   } else {
-    browser.notifications.clear("pollNotifierNotification");
+    browser.notifications.clear(notificationId);
   }
 });
